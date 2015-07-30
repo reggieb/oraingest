99888ff8073ce819271d5273ae87a187d27f7b7d
Fred Heath <fred_h@bootstrap.me.uk>
2015-07-30 15:13:09 +0100

    added secret_key_base to eliminate deprecation error when testing

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 OraHydra::Application.config.secret_token = 'ae67093fed1eadb55acebf660d1e0803bdae5e1d68f6d381dd78940c8722a3e52cb91eba75654a708d1ef59294b8080cbb709924845bbfc46470196064729079'
+OraHydra::Application.config.secret_key_base = 'cfe6d88143343c9133acf6eb45aa7f88981797afd465b96ded40db523d9e67279ecb1672da7dfcf6487ba621e616a5bbd033267b67d5609b4d83e470ad9fcf4e'
