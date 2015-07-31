710a9ee9e4153da758c6089a290e9f3ae859aafc
Fred Heath <fred_h@bootstrap.me.uk>
2015-07-31 13:43:07 +0100

    changed integration tests for solr and fedora

--- test/integration/test_dependent_services.rb
+++ test/integration/test_dependent_services.rb
@@ -15,31 +15,37 @@ class TestDependentServices < Test::Unit::TestCase
     when "test"
       @port = ENV['JETTY_PORT']
     end
+
+    puts "================= port used: #{@port} ==============="
   end
 
-  def test_java_is_installed
+  # def test_java_is_installed
 
-    stdin, stdout, stderr = Open3.popen3('java -version')
-    refute(stderr.read.empty?)
-  end
+  #   stdin, stdout, stderr = Open3.popen3('java -version')
+  #   refute(stderr.read.empty?)
+  # end
 
-  def test_apache_is_running
-    stdin, stdout, stderr = Open3.popen3('sudo service apache2 status')
-    assert_not_nil(stdout.read.match /apache2 is running/)
-  end
+  # def test_apache_is_running
+  #   stdin, stdout, stderr = Open3.popen3('sudo service apache2 status')
+  #   assert_not_nil(stdout.read.match /apache2 is running/)
+  # end
 
-  def test_tomcat_is_running
-    stdin, stdout, stderr = Open3.popen3('sudo service tomcat7 status')
-    assert_not_nil(stdout.read.match /Tomcat servlet engine is running/)
-  end  
+  # def test_tomcat_is_running
+  #   stdin, stdout, stderr = Open3.popen3('sudo service tomcat7 status')
+  #   assert_not_nil(stdout.read.match /Tomcat servlet engine is running/)
+  # end  
 
   def test_solr_is_running
-    assert_not_nil(@port, "ENV['SOLR_PORT'] expected to be not nil!")
     uri = URI.parse("http://127.0.0.1:#{@port}/solr/#/#{Rails.env}")
     res = Net::HTTP.get_response(uri)
     assert(res.is_a? Net::HTTPOK)
   end
 
+  def test_fedora_is_running
+    uri = URI.parse("http://127.0.0.1:#{@port}/solr/#/#{Rails.env}")
+    res = Net::HTTP.get_response(uri)
+    assert(res.is_a? Net::HTTPOK)
+  end
 
 
   def teardown
