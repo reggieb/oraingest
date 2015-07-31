6102dedaaceb1c4bcbede28a1400cd620f4488ec
Fred Heath <fred_h@bootstrap.me.uk>
2015-07-31 13:25:25 +0100

    undo previous commit

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,6 +4,6 @@ require "rdf"
 class Person < ActiveFedora::Base
   #include Sufia::GenericFile
   has_metadata :name => "descMetadata", :type => PersonRdfDatastream
-  has_attributes :descMetadata, [:first_name, :last_name, :display_name, :name, :title, :email, 
+  delegate_to :descMetadata, [:first_name, :last_name, :display_name, :name, :title, :email, 
   :website, :institution, :faculty, :oxford_college, :research_group, :webauth, :identifier]
 end
