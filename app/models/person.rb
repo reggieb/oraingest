70c36ffc3cf0f594eccfcb093022241582cdede6
Rob Nichols <rob@nicholshayes.co.uk>
2015-08-14 10:38:10 +0100

    Fixes deprecation issues caused by using delegate_to

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,16 @@
-require "datastreams/person_rdf_datastream"
-require "rdf"
+require 'datastreams/person_rdf_datastream'
+require 'rdf'
 
 class Person < ActiveFedora::Base
   #include Sufia::GenericFile
-  has_metadata :name => "descMetadata", :type => PersonRdfDatastream
-  delegate_to :descMetadata, [:first_name, :last_name, :display_name, :name, :title, :email, 
-  :website, :institution, :faculty, :oxford_college, :research_group, :webauth, :identifier]
+  has_metadata name: 'descMetadata', type: PersonRdfDatastream
+  delegate_to(
+    :descMetadata, 
+    [
+      :first_name, :last_name, :display_name, :name, :title, :email, 
+      :website, :institution, :faculty, :oxford_college, :research_group, 
+      :webauth, :identifier
+    ],
+    multiple: true
+  )
 end
