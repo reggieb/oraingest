7720f04c009e6ee9e2b543c0f8215256805e8cdd
Fred Heath <fred_h@bootstrap.me.uk>
2015-09-29 23:16:56 +0100

    added article content to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,13 +6,15 @@ class DashboardController < ApplicationController
  params = {qt: "search", rows: 10, "spellcheck.q"=>nil, facet: true, :"facet.field"=>["desc_metadata__type_sim", "MediatedSubmission_status_ssim", "desc_metadata__creator_sim", "desc_metadata__keyword_sim", "desc_metadata__subject_sim", "desc_metadata__language_sim", "desc_metadata__based_near_sim", "desc_metadata__publisher_sim", "active_fedora_model_ssi", "MediatedSubmission_current_reviewer_id_ssim", "MediatedSubmission_all_reviewer_ids_ssim"], :"f.desc_metadata__type_sim.facet.limit"=>11, :"f.MediatedSubmission_status_ssim.facet.limit"=>16, :"f.desc_metadata__creator_sim.facet.limit"=>11, :"f.desc_metadata__keyword_sim.facet.limit"=>11, :"f.desc_metadata__subject_sim.facet.limit"=>11, :"f.desc_metadata__language_sim.facet.limit"=>6, :"f.desc_metadata__based_near_sim.facet.limit"=>11, :"f.desc_metadata__publisher_sim.facet.limit"=>11, :"f.active_fedora_model_ssi.facet.limit"=>11, :"f.MediatedSubmission_current_reviewer_id_ssim.facet.limit"=>6, :"f.MediatedSubmission_all_reviewer_ids_ssim.facet.limit"=>6, sort: "score desc, system_create_dtsi desc"}#, 
  	 # fq: ["active_fedora_model_ssi:Article OR active_fedora_model_ssi:Dataset", "-MediatedSubmission_status_ssim:Draft"]}
 
-    search_params = { q: "depositor_tesim:bodl2339",
+    search_params = { q: "desc_metadata__title_tesim:\"disaster\"",
   					start: 0,
   					rows: 10
   					}
-    # search_params = {q: "id:uuid:d640713e-9a34-4ab2-afcf-317dc5f5af12"}
-    # @response =  s.search params.merge(q: 'desc_metadata__contributor_tesim:bodl1087')
-    @response =  s.search search_params
+
+    response =  s.search search_params
+    @number_items_found =  response['response']['numFound']
+    @result_list = response['response']['docs'] if @number_items_found > 0
+
 
   end
 
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,12 @@
 module DashboardHelper
+
+  def get_type_tag(item_type)
+  	item_type.downcase.gsub(%r{\s}, '-').prepend('tag-')
+  end
+
+  def get_status_tag(item_status)
+  	item_status.downcase.gsub(%r{\s}, '-').prepend('tag-')
+  end
+
+
 end
