38f59f52254bfdfe844ff571868c4d8b481f73ed
Fred Heath <fred_h@bootstrap.me.uk>
2015-10-01 14:44:22 +0100

    added pagination

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,22 +1,27 @@
+    class RSolr::Response::PaginatedDocSet
+    	attr_reader :limit_value
+    end
+
 class DashboardController < ApplicationController
   def index
     s = SolrSearch.new
-
- params = {qt: "search", rows: 10, "spellcheck.q"=>nil, facet: true, :"facet.field"=>["desc_metadata__type_sim", "MediatedSubmission_status_ssim", "desc_metadata__creator_sim", "desc_metadata__keyword_sim", "desc_metadata__subject_sim", "desc_metadata__language_sim", "desc_metadata__based_near_sim", "desc_metadata__publisher_sim", "active_fedora_model_ssi", "MediatedSubmission_current_reviewer_id_ssim", "MediatedSubmission_all_reviewer_ids_ssim"], :"f.desc_metadata__type_sim.facet.limit"=>11, :"f.MediatedSubmission_status_ssim.facet.limit"=>16, :"f.desc_metadata__creator_sim.facet.limit"=>11, :"f.desc_metadata__keyword_sim.facet.limit"=>11, :"f.desc_metadata__subject_sim.facet.limit"=>11, :"f.desc_metadata__language_sim.facet.limit"=>6, :"f.desc_metadata__based_near_sim.facet.limit"=>11, :"f.desc_metadata__publisher_sim.facet.limit"=>11, :"f.active_fedora_model_ssi.facet.limit"=>11, :"f.MediatedSubmission_current_reviewer_id_ssim.facet.limit"=>6, :"f.MediatedSubmission_all_reviewer_ids_ssim.facet.limit"=>6, sort: "score desc, system_create_dtsi desc"}#, 
- 	 # fq: ["active_fedora_model_ssi:Article OR active_fedora_model_ssi:Dataset", "-MediatedSubmission_status_ssim:Draft"]}
 
     search_params = { q: "desc_metadata__title_tesim:\"disaster\"",
   					start: 0,
   					rows: 10
   					}
+    response =  s.search(search_params, params[:page])
 
-    response =  s.search(search_params)
-    logger.debug "*******************"
-    logger.debug params[:page]
-    logger.debug "*******************"
+
+    @enable_search_form = false #stop ora search form appearing
     @number_items_found =  response['response']['numFound']
-    @result_list = response['response']['docs'] if @number_items_found > 0
-
+    if @number_items_found > 0
+    	binding.pry
+    	 fc = response['facet_counts']['facet_fields']
+	    @result_list = response['response']['docs'] 
+	else
+		# TODO deal with error
+	end
 
 
   end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -5,9 +5,10 @@ module DashboardHelper
   #
   # @note returned tag must match the one found in styles.scss:
   # @param review_item [Hash] the review item (dataset/thesis/etc) to be processed
-  # @return  date_list [ Array ] first element is the type of date, second is the actual date
+  # @return  [ String ] the generated tag, or an empty string if the argument is nil
   def get_type_tag(item_type)
-    item_type.downcase.gsub(%r{\s}, '-').prepend('tag-')
+    item_type ? item_type.downcase.gsub(%r{\s}, '-').prepend('tag-')
+    : ""
   end
 
   # uses the status of an item to generate a corresponding CSS tag to be used # in the dashboard view
@@ -16,7 +17,8 @@ module DashboardHelper
   # @param (see #get_type_tag)
   # @return (see #get_type_tag)
   def get_status_tag(item_status)
-    item_status.downcase.gsub(%r{\s}, '-').prepend('tag-')
+    item_status ? item_status.downcase.gsub(%r{\s}, '-').prepend('tag-')
+    : ""
   end
 
 
@@ -26,11 +28,14 @@ module DashboardHelper
   # @param limit [FixNum] the limit to trim to
   # @return [String] the trimmed or untrimmed text
   def trim_text(txt, limit)
-    txt = txt.size > limit ?
-      (txt.slice(0, limit ) + "...") :
-      txt
+    if txt
+      txt = (txt.size > limit) ?
+        (txt.slice(0, limit ) + "...") :
+        txt
+    else
+      txt = ""
+    end
   end
-
 
   # examines a review item to determine which date can be displayed on teh dashboard view
   #
@@ -38,19 +43,21 @@ module DashboardHelper
   # @return  date_list [ Array ] first element is the type of date, second is the actual date
   def determine_date(review_item)
     date_list = []
-    if review_item['desc_metadata__datePublished_tesim']
-      date_list = ['published', review_item['desc_metadata__datePublished_tesim'].first]
-    elsif
-      review_item['desc_metadata__dateAccepted_tesim']
-      date_list = ['accepted', review_item['desc_metadata__dateAccepted_tesim'].first]
-    else
-      date_list = ['not_found']
-    end
+    if review_item
+	    if review_item['desc_metadata__datePublished_tesim']
+	      date_list = ['published', review_item['desc_metadata__datePublished_tesim'].first]
+	    elsif
+	      review_item['desc_metadata__dateAccepted_tesim']
+	      date_list = ['accepted', review_item['desc_metadata__dateAccepted_tesim'].first]
+	    else
+	      date_list = ['not_found']
+	    end
+	end
     date_list
   end
 
   def is_item_claimed?(review_item)
-    review_item['MediatedSubmission_status_ssim'].first
+    review_item ? review_item['MediatedSubmission_status_ssim'].first : false
   end
 
 end
